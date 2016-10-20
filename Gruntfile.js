4b94037d0d5e0c74b8bf403e20efcf55f93858a3
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-10-20 11:56:47 +0300

    build: copy imgs to dist.

--- Gruntfile.js
+++ Gruntfile.js
@@ -369,6 +369,7 @@ module.exports = function (grunt) {
             'bower_components/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
+            'assets/images/*',
             'index.html'
           ]
         }, {

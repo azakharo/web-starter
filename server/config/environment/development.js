d27619f5f01cece6e0a3ea8ffa4628561faab909
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2017-02-28 12:13:12 +0300

    Rem node-inspector. Temporary switch dev to mlab.

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,8 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/projects-dev'
+    //uri: 'mongodb://localhost/projects-dev'
+    uri: 'mongodb://admin:admin@ds029575.mlab.com:29575/web-starter'
   },
 
   seedDB: true

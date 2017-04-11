c73f9fe0bf4c157667a7f01466a0472be038bfcb
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2017-04-11 17:18:34 +0300

    Rem method-override.

--- server/config/express.js
+++ server/config/express.js
@@ -9,7 +9,6 @@ var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
 var cookieParser = require('cookie-parser');
 var path = require('path');
 var config = require('./environment');
@@ -26,7 +25,6 @@ module.exports = function(app) {
   app.use(compression());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
-  app.use(methodOverride());
   app.use(cookieParser());
   app.use(passport.initialize());
 
